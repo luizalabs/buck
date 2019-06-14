fb5da17cb4a70b27fa2f1e4bc0687ec9e48266c5
Thiago Cruz <thiago.ferrao91@gmail.com>
2019-06-14 16:23:26 -0300

    Handle resource dependencies of static prebuilt frameworks by rmaz

--- src/com/facebook/buck/features/apple/project/ProjectGenerator.java
+++ src/com/facebook/buck/features/apple/project/ProjectGenerator.java
@@ -921,18 +921,6 @@ public class ProjectGenerator {
                       filteredRules.add(node);
                     }
                   });
-          TargetNodes.castArg(node, PrebuiltAppleFrameworkDescriptionArg.class)
-              .ifPresent(
-                  prebuiltFramework -> {
-                    // Technically (see Apple Tech Notes 2435), static frameworks are lies. In case
-                    // a static framework is used, they can escape the incorrect project generation
-                    // by marking its preferred linkage static (what does preferred linkage even
-                    // mean for a prebuilt thing? none of this makes sense anyways).
-                    if (prebuiltFramework.getConstructorArg().getPreferredLinkage()
-                        != NativeLinkableGroup.Linkage.STATIC) {
-                      filteredRules.add(node);
-                    }
-                  });
         }
       }
     } catch (AcyclicDepthFirstPostOrderTraversal.CycleException e) {
@@ -3367,6 +3355,11 @@ public class ProjectGenerator {
       return Optional.of(
           CopyFilePhaseDestinationSpec.of(PBXCopyFilesBuildPhase.Destination.RESOURCES));
     } else if (targetNode.getDescription() instanceof PrebuiltAppleFrameworkDescription) {
+      PrebuiltAppleFrameworkDescriptionArg frameworkDescriptionArg =
+          (PrebuiltAppleFrameworkDescriptionArg) targetNode.getConstructorArg();
+      if (frameworkDescriptionArg.getPreferredLinkage() == NativeLinkable.Linkage.STATIC) {
+        return Optional.empty();
+      }
       return Optional.of(
           CopyFilePhaseDestinationSpec.of(PBXCopyFilesBuildPhase.Destination.FRAMEWORKS));
     } else if (targetNode.getDescription() instanceof PrebuiltCxxLibraryDescription) {
